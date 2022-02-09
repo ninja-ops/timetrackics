418c5da6f0f1fac26e223208d52d762a2f2d45a0
ninja-ops <bench2600@gmail.com>
2022-02-09 18:03:39 +0100

    init

---
+++ timetrackics.py
@@ -0,0 +1,121 @@
+import os.path, sys, argparse
+from icalendar import Calendar, Event
+from datetime import datetime
+from pytz import UTC
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("filename", help="location of icalendar file")
+  parser.add_argument("--category", help="filter events by category", default="")
+  parser.add_argument("--location", help="filter events by location", default="")
+  parser.add_argument("--description", help="filter events for something whats in description", default="")
+  parser.add_argument("--date", help="only add events where date starts with this, e.g. 2003-11", default="")
+  parser.add_argument("--header", help="set to something enables heading row", default="1")
+  parser.add_argument("--debug", help="set to something enables debug mode", default="")
+  parser.add_argument("--sum", help="prints sum hour's at the end", default="")
+  args = parser.parse_args()
+  if os.path.isfile(args.filename) == False:
+    print("file '" + args.filename + "' does not exists")
+    sys.exit(2)
+
+  lines = []
+
+
+  g = open(args.filename,'rb')
+  gcal = Calendar.from_ical(g.read())
+
+  sum_hours = 0
+
+  for component in gcal.walk():
+    if component.name == "VEVENT":
+      add = True
+
+      dtstart = component.get('dtstart').dt
+      time_dtstart = datetime.fromisoformat(str(dtstart))
+
+      dtend = component.get('dtend').dt
+      time_dtend = datetime.fromisoformat(str(dtend))
+      time_diff = time_dtend - time_dtstart
+      time_hours = time_diff.total_seconds()/60/60
+
+      categories = component.get('categories')
+      summary = component.get('summary')
+      location = component.get('location')
+      description = component.get('description')
+
+      if categories != None and args.category != "":
+        if categories.to_ical().decode() != args.category:
+          add = False
+      else:
+        if args.category != "":
+          add = False
+
+      if categories != None:
+        categories_string = categories.to_ical().decode()
+      else:
+        categories_string = ""
+
+      if location != None and args.location != "":
+        if location != args.location:
+          add = False
+      else:
+        if args.location != "":
+          add = False
+
+      if location == None:
+        location = ""
+
+      if description != None and args.description != "":
+        if description.find(args.description) == -1:
+          add = False
+      else:
+        if args.description != "":
+          add = False
+
+      if args.date != "":
+        if str(dtstart).find(args.date) != 0:
+          add = False
+
+      if args.debug != "":
+        print("DEBUG DTSTART\t\t:", dtstart)
+        print("DEBUG DTEND\t\t:", dtend)
+        print("DEBUG SUMMARY\t\t:", summary)
+        print("DEBUG LOCATION\t\t:", location)
+        print("DEBUG DESCRIPTION \t:", str(description).replace("\n", " "))
+        print("DEBUG HOURS\t\t:", time_hours)
+
+
+      if add == True:
+        sum_hours = sum_hours + time_hours
+        if args.debug != "":
+          print("DEBUG ?\t\t\t! add to csv-string")
+          print("")
+        line = str(dtstart) + "\t" + str(dtend) + "\t" + str(time_hours) + "\t" + location + "\t" + categories_string + "\t" + summary + "\t" + str(description).replace("\n", " ")
+        lines.append(line)
+      else:
+        if args.debug != "":
+          print("DEBUG ?\t\t\t! dont add to csv-string")
+          print("")
+
+  g.close()
+
+  if args.debug != "":
+    print("DEBUG ", lines)
+    print()
+
+
+  if args.header != "":
+    print("start\tend\thours\tlocation\tcategory\tsummary\tdescription")
+
+  lines.sort(reverse=False)
+  for line in lines:
+    print(line)
+
+  if args.sum != "":
+    print()
+    print("==============")
+    print("Hours\t",sum_hours)
+    print("==============")
+    print()
+
+main()
